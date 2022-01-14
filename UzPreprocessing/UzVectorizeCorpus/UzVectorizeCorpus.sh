2b278a603cfa1f937f6d682313819bb2b99abdc3
Maxim <46705352+mh1m@users.noreply.github.com>
2022-01-15 02:00:58 +0300

    Delete UzVectorizeCorpus.sh

--- UzPreprocessing/UzVectorizeCorpus/UzVectorizeCorpus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -c 10
-#SBATCH --time=720:00:00
-#SBATCH --output=/home/nsborodin/Maksim/output/SvVectorizeCorpus.log
-
-source /home/nsborodin/Maksim/my_env/bin/activate
-python SvVectorizeCorpus.py
