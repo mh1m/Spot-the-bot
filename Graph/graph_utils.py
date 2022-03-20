fda6319c676adb5874b1b4405f607b399b6d4c27
Maxim Seleznev <mrseleznev@edu.hse.ru>
2022-03-20 19:48:52 +0300

    graph utils

--- Graph/graph_utils.py
+++ Graph/graph_utils.py
@@ -1,29 +1,60 @@
+import ray
 import copy
 import math
 import scipy
+import psutil
 import pickle
 import itertools
 import numpy as np
 import pandas as pd
+import multiprocessing
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from scipy.spatial import Delaunay
+from joblib import Parallel, delayed
 
 
-def delete_nans(corpus: list) -> list:
-    result_corpus = []
-    for text in tqdm(corpus):
-        dict_ = {key: value for key, value in text.items() if value is not None}
-        result_corpus.append(dict_)
-    return result_corpus
-
-def create_vertices(text: list, vector_shape: int) -> dict:
+def create_vertices_with_sentences(corpus: list, k: int) -> dict:
+    '''
+    Create vertices from vectorized corpus splitted by sentences
+    
+    Parameters
+    ----------
+    corpus : list
+    Vectorized corpus = [document_1, ..., document_i ..., document_n], where
+    text_i = [{'document_index': i,
+               'sentence_index': 0,
+               'sentence_text': {word: vector, 
+                                 word: vector, ...}},
+              ...,
+              {'document_index': i,
+               'sentence_index': m_i,
+               'sentence_text': {word: vector, 
+                                 word: vector, ...}}]
+    So, we get n documents and m_i sentences for each document
+    
+    k : int
+    Vector length
+    
+    Returns
+    -------
+    vertices : dict
+    Returns dict = {word: Node(vector,
+                               empty set, 
+                               set of words that occur in the same sentence throughout the corpus)}
+    '''
     vertices = dict()
-    for text in tqdm(text):
-        for word, vector in text.items():
-            if word not in vertices.keys():
-                vertices[word] = Node(vector[:vector_shape], set())
+    for text in tqdm(corpus):
+        for sentence in text:
+            for word, vector in sentence['sentence_text'].items():
+                word_neighbors = set(sentence['sentence_text'].keys()).difference(set([word]))
+                if word in vertices.keys():
+                    vertices[word].sentence_neighbors.update(word_neighbors)
+                else:
+                    vertices[word] = Node(vector=np.array(vector[:k]),
+                                          neighbors=set(),
+                                          sentence_neighbors=word_neighbors)
     return vertices
 
 
@@ -32,9 +63,10 @@ Node and Graph
 '''
 
 class Node:
-    def __init__(self, vector: np.ndarray, neighbours: set):
+    def __init__(self, vector: np.ndarray, neighbors: set, sentence_neighbors: set):
         self.__vector = copy.deepcopy(vector)
-        self.__neighbours = copy.deepcopy(neighbours)
+        self.__neighbors = copy.deepcopy(neighbors)
+        self.__sentence_neighbors = copy.deepcopy(sentence_neighbors)
     
     @property
     def vector(self):
@@ -43,15 +75,22 @@ class Node:
     @vector.setter
     def vector(self, vector: np.ndarray):
         self.__vector = copy.deepcopy(vector)
-    
+            
     @property
-    def neighbours(self):
-        return self.__neighbours
+    def neighbors(self):
+        return self.__neighbors
     
-    @neighbours.setter
-    def neighbours(self, neighbours: set):
-        self.__neighbours = copy.deepcopy(neighbours)
-
+    @neighbors.setter
+    def neighbors(self, neighbors: set):
+        self.__neighbors = copy.deepcopy(neighbors)
+        
+    @property
+    def sentence_neighbors(self):
+        return self.__sentence_neighbors
+    
+    @sentence_neighbors.setter
+    def sentence_neighbors(self, sentence_neighbors: set):
+        self.__sentence_neighbors = sentence_neighbors
 
 class Graph:
     def __init__(self, vertices: dict):
@@ -64,6 +103,9 @@ class Graph:
     @vertices.setter
     def vertices(self, vertices: dict):
         self.__vertices = copy.deepcopy(vertices)
+        
+    def get_words(self) -> list:
+        return list(self.vertices.keys())
     
     def get_vectors(self) -> list:
         vectors = list()
@@ -71,61 +113,63 @@ class Graph:
             vectors.append(node.vector.tolist())
         return vectors
     
-    def get_words(self) -> list:
-        words = list()
-        for word in self.vertices.keys():
-            words.append(word)
-        return words
-    
     def get_edges(self) -> set:
         edges = set()
         for word, node in self.vertices.items():
-            for neighbour in node.neighbours:
-                if (word, neighbour) in edges or (neighbour, word) in edges:
+            for neighbor in node.neighbors:
+                if (word, neighbor) in edges or (neighbor, word) in edges:
                     continue
-                edges.add((word, neighbour))
+                edges.add((word, neighbor))
         return edges
     
     def get_other_words(self, word: str) -> set:
-        return set(self.vertices.keys()).difference(set([word]))
-    
-    def delete_edge(self, first_word: str, second_word: str):
-        self.vertices[first_word].neighbours.difference_update(set([second_word]))
-        self.vertices[second_word].neighbours.difference_update(set([first_word]))
+        return set(self.get_words()).difference(set([word]))
     
     def add_edge(self, first_word: str, second_word: str):
-        self.vertices[first_word].neighbours.update(set([second_word]))
-        self.vertices[second_word].neighbours.update(set([first_word]))
+        self.vertices[first_word].neighbors.add(second_word)
+        self.vertices[second_word].neighbors.add(first_word)
     
-    def reset_graph_neighbours(self):
+    def delete_edge(self, first_word: str, second_word: str):
+        self.vertices[first_word].neighbors.difference_update(set([second_word]))
+        self.vertices[second_word].neighbors.difference_update(set([first_word]))
+    
+    def reset_graph_neighbors(self):
         for node in self.vertices.values():
-            node.neighbours.clear()
+            node.neighbors.clear()
     
     def euclid_distance(self, first_word: str, second_word: str) -> np.float64:
         first_vertex = self.vertices[first_word].vector
         second_vertex = self.vertices[second_word].vector
         return np.linalg.norm(first_vertex - second_vertex)
     
+    def euclid_distance_to_vector(self, word: str, vector: np.ndarray) -> np.float64:
+        word_vector = self.vertices[word].vector
+        return np.linalg.norm(word_vector - vector)
+    
     def get_knn(self, word: str, k: int) -> list:
         distance_dict = dict()
-        other_words = self.get_other_words(word)
-        for other_word in other_words:
+        for other_word in self.get_other_words(word):
             distance_dict[other_word] = self.euclid_distance(word, other_word)
         lambda_ = lambda item: item[1]
         words = [key for key, value in sorted(distance_dict.items(), key=lambda_)]
         return words[:k]
     
     def get_sphere_radius(self, word: str) -> float:
-        return self.euclid_distance(word, self.get_knn(word, 1)[0])
+        nearest_neighbor = self.get_knn(word, 1)[0]
+        return self.euclid_distance(word, nearest_neighbor)
     
     def in_sphere_check(self, first_word: str, second_word: str) -> bool:
-        nearest_neighbour = self.get_knn(first_word, 1)
-        radius = self.euclid_distance(first_word, nearest_neighbour)
+        nearest_neighbor = self.get_knn(first_word, 1)
+        radius = get_sphere_radius(first_word)
         distance = self.euclid_distance(first_word, second_word)
-        if radius < distance:
+        if distance > radius:
             return False
         return True
     
+    def update_graph_by_sentence_neighbors(self):
+        for word in tqdm(self.vertices.keys()):
+            self.vertices[word].neighbors.intersection_update(self.vertices[word].sentence_neighbors)
+
     def plot_graph(self,
                figsize=(60, 60),
                dpi=120,
@@ -151,18 +195,50 @@ class Graph:
 Граф ε-окружности (ε-ball)
 '''
 
-class EBall(Graph):
+@ray.remote
+class EBallParallel(Graph):
     def __init__(self, vertices: dict):
         super().__init__(vertices)
-    
-    def create_eball_graph(self, epsilon: int):
-        self.reset_graph_neighbours()
-        for first_word in tqdm(self.vertices.keys()):
+        self.parallel = set()
+        
+    def add_parallel_word(self, word: str):
+        self.parallel.add(word)
+                    
+    def parallel_eball_graph(self, epsilon: float) -> dict:
+        self.reset_graph_neighbors()
+        for first_word in tqdm(self.parallel):
             other_words = self.get_other_words(first_word)
             for second_word in other_words:
                 distance = self.euclid_distance(first_word, second_word)
                 if distance < epsilon:
                     self.add_edge(first_word, second_word)
+        return self.vertices
+    
+    
+class EBall(Graph):
+    def __init__(self, vertices: dict):
+        super().__init__(vertices)
+        
+    def create_eball_graph(self, epsilon: float):
+        self.reset_graph_neighbors()
+        for first_word in tqdm(self.get_words()):
+            other_words = self.get_other_words(first_word)
+            for second_word in other_words:
+                distance = self.euclid_distance(first_word, second_word)
+                if distance < epsilon:
+                    self.add_edge(first_word, second_word)
+    
+    def create_parallel_eball_graph(self, epsilon: float, num_cpus: int):
+        self.reset_graph_neighbors()
+        streaming_actors = [EBallParallel.remote(self.vertices) for _ in range(num_cpus)]
+        for index, word in enumerate(self.get_words()):
+            streaming_actors[index % num_cpus].add_parallel_word.remote(word)
+        results = ray.get([
+            actor.parallel_eball_graph.remote(epsilon) for actor in streaming_actors
+        ])
+        for parallel_vertices in results:
+            for word in self.get_words():
+                self.vertices[word].neighbors.update(parallel_vertices[word].neighbors)
 
 
 '''
@@ -196,35 +272,44 @@ class GG(Graph):
         self.__delaunay = copy.deepcopy(delaunay)
     
     def create_delaunay_graph(self):
-        self.reset_graph_neighbours()
+        self.reset_graph_neighbors()
         vectors = self.get_vectors()
         words = self.get_words()
         word_num_dict = {word: num for word, num in enumerate(words)}
         self.delaunay = Delaunay(np.array(vectors))
         delaunay_graph = self.delaunay.simplices.tolist()
-        for triangle in delaunay_graph:
+        for triangle in tqdm(delaunay_graph):
             triangle_words = set(map(word_num_dict.get, triangle))
             self.triangles.append(triangle_words)
             for word in triangle_words:
-                new_neighbours = triangle_words.difference(set([word]))
-                self.vertices[word].neighbours.update(new_neighbours)
+                new_neighbors = triangle_words.difference(set([word]))
+                self.vertices[word].neighbors.update(new_neighbors)
     
-    def in_edge_sphere(self, edge: tuple, neighbour: str) -> bool:
+    def is_neighbor_in_edge_sphere_gromov(self, edge: tuple, neighbor: str) -> bool:
         edge_length = self.euclid_distance(edge[0], edge[1])
-        first_to_neighbour = self.euclid_distance(edge[0], neighbour)
-        second_to_neighbour = self.euclid_distance(edge[1], neighbour)
-        if edge_length < math.sqrt(first_to_neighbour ** 2 + second_to_neighbour ** 2):
+        first_to_neighbor = self.euclid_distance(edge[0], neighbor)
+        second_to_neighbor = self.euclid_distance(edge[1], neighbor)
+        if edge_length < math.sqrt(first_to_neighbor ** 2 + second_to_neighbor ** 2):
             return False
         return True
     
+    def is_neighbor_in_edge_sphere(self, edge: tuple, neighbor: str) -> bool:
+        sphere_center = (self.vertices[edge[0]].vector + self.vertices[edge[1]].vector) / 2
+        radius = self.euclid_distance_to_vector(edge[0], sphere_center)
+        if self.euclid_distance_to_vector(neighbor, sphere_center) < radius:
+            return True
+        return False
+    
     def create_gabriel_graph(self):
+        print("delaunay start...")
         self.create_delaunay_graph()
-        for triangle in self.triangles:
+        print("delaunay done!")
+        for triangle in tqdm(self.triangles):
             edges = list(itertools.combinations(triangle, 2))
             for edge in edges:
-                neighbours = triangle.difference(set(edge))
-                for neighbour in neighbours:
-                    if self.in_edge_sphere(edge, neighbour):
+                neighbors = triangle.difference(set(edge))
+                for neighbor in neighbors:
+                    if self.is_neighbor_in_edge_sphere(edge, neighbor):
                         self.delete_edge(edge[0], edge[1])
 
 
@@ -243,16 +328,16 @@ class RNG(GG):
     def create_rn_graph(self):
         self.create_gabriel_graph()
         for word in tqdm(self.vertices.keys()):
-            neighbours = copy.deepcopy(self.vertices[word].neighbours)
-            for neighbour in neighbours:
+            neighbors = copy.deepcopy(self.vertices[word].neighbors)
+            for neighbor in neighbors:
                 for other_word in self.get_other_words(word):
-                    if neighbour == other_word:
+                    if neighbor == other_word:
                         continue
-                    word_to_neighbour_distance = self.euclid_distance(word, neighbour)
+                    word_to_neighbor_distance = self.euclid_distance(word, neighbor)
                     word_to_other_word_distance = self.euclid_distance(word, other_word)
-                    neighbour_to_other_word_distance = self.euclid_distance(neighbour, other_word)
-                    if max(word_to_other_word_distance, neighbour_to_other_word_distance) <= word_to_neighbour_distance:
-                        self.delete_edge(word, neighbour)
+                    neighbor_to_other_word_distance = self.euclid_distance(neighbor, other_word)
+                    if max(word_to_other_word_distance, neighbor_to_other_word_distance) <= word_to_neighbor_distance:
+                        self.delete_edge(word, neighbor)
 
 
 '''
@@ -265,7 +350,7 @@ class IG(Graph):
         super().__init__(vertices)
     
     def create_influence_graph(self):
-        self.reset_graph_neighbours()
+        self.reset_graph_neighbors()
         for first_word in tqdm(self.vertices.keys()):
             other_words = self.get_other_words(first_word)
             first_radius = self.get_sphere_radius(first_word)
@@ -286,8 +371,8 @@ class NNG(Graph):
         super().__init__(vertices)
     
     def create_nn_graph(self, k: int):
-        self.reset_graph_neighbours()
+        self.reset_graph_neighbors()
         for word in tqdm(self.vertices.keys()):
             knn_list = self.get_knn(word, k)
-            for neighbour in knn_list:
-                self.add_edge(word, neighbour)
+            for neighbor in knn_list:
+                self.add_edge(word, neighbor)
